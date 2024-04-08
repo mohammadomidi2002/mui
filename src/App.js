0668fadc56e8f02612207878ecceb092cea79c18
mohammad.omidi <mohammadomidi.developer@gmail.com>
2024-04-08 16:37:46 +0330

    create theme (custom color)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
+import { Button, Typography, styled } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import SettingsIcon from "@mui/icons-material/Add";
 
 function App() {
+  const BlueButton = styled(Button)({
+    backgroundColor: "skyblue",
+    color: "#888",
+    margin: 5,
+    "&:hover": {
+      backgroundColor: "lightblue",
+    },
+    "&:disabled": {
+      backgroundColor: "gray",
+      color: "white",
+    },
+  });
   return (
     <div>
-      hello
+      <Button variant="text">TEXT</Button>
+      <Button
+        variant="contained"
+        startIcon={<SettingsIcon />}
+        color="otherColor"
+        size="small"
+      >
+        SETTING
+      </Button>
+      <Button variant="contained" startIcon={<AddIcon />} color="success">
+        ADD NEW POST
+      </Button>
+      <Button variant="outlined" disabled>
+        OUTLINED
+      </Button>
+      <Typography variant="h1" component="p">
+        It uses h1 style but it's p tag
+      </Typography>
+      <BlueButton>MY UNIQUE BlueButton</BlueButton>
+      <BlueButton>Another BlueButton</BlueButton>
     </div>
   );
 }
---
+++ src/theme.js
@@ -0,0 +1,35 @@
+// import { createTheme } from "@mui/material";
+
+// export const theme = createTheme({
+//     pallets: {
+//         primary:{
+//             main: "#1760a5",
+//             light: "skyblue"
+//         },
+//         secondary: {
+//             main: "#15c630",
+//         },
+//         otherColor : {
+//             main: "#999"
+//         }
+//     }
+// })
+
+
+
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1760a5",
+      light: "skyblue"
+    },
+    secondary: {
+      main: "#15c630",
+    },
+    otherColor : {
+      main: "#999"
+    }
+  }
+})
